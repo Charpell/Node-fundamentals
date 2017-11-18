45d90654046a796768a7a9464d18deff82e088e3
charpell <charpellumeh@gmail.com>
2017-11-18 09:02:17 +0100

    2

--- app.js
+++ app.js
@@ -4,8 +4,11 @@ console.log('Starting app.')
 const fs = require('fs')
 const os = require('os')
 const notes = require('./notes')
+const lodash = require('lodash')
 
-console.log('Result:', notes.add(2,3))
+console.log(lodash.isString('And'))
+var filteredArray = lodash.uniq([2,2,3,1,4,2])
+console.log(filteredArray)
 
 // var res = notes.addNote()
 // console.log(res)
